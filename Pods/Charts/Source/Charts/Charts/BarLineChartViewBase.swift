b6cab7ffad6313e789b04e30195c9dcceb3337f1
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2020-04-27 05:38:29 +0000

    Format with prettier

--- Pods/Charts/Source/Charts/Charts/BarLineChartViewBase.swift
+++ Pods/Charts/Source/Charts/Charts/BarLineChartViewBase.swift
@@ -3,4 +3,22 @@
 //  Charts
 //
 //  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
-//  A port 
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+#if canImport(UIKit)
+    import UIKit
+#endif
+
+#if canImport(Cocoa)
+import Cocoa
+#endif
+
+/// Base-class of LineChart, BarChart, ScatterChart and CandleStickChart.
+open class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChartDataProvider, NSUIGestureRecognizerDele
