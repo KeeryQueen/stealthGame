0de2670d243415652bf106f37ee01001b701b80f
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2020-04-29 18:51:40 +0000

    Update monitor.py

--- Pods/Charts/Source/Charts/Charts/BarLineChartViewBase.swift
+++ Pods/Charts/Source/Charts/Charts/BarLineChartViewBase.swift
@@ -21,4 +21,16 @@ import Cocoa
 #endif
 
 /// Base-class of LineChart, BarChart, ScatterChart and CandleStickChart.
-open class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChartDataProvider, NSUIGestureRecognizerDele
+open class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChartDataProvider, NSUIGestureRecognizerDelegate
+{
+    /// the maximum number of entries to which values will be drawn
+    /// (entry numbers greater than this value will cause value-labels to disappear)
+    internal var _maxVisibleCount = 100
+    
+    /// flag that indicates if auto scaling on the y axis is enabled
+    private var _autoScaleMinMaxEnabled = false
+    
+    private var _pinchZoomEnabled = false
+    private var _doubleTapToZoomEnabled = true
+    private var _dragXEnabled = true
+    
