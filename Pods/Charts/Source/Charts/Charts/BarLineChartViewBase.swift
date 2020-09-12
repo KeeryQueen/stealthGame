ceb7c8f856f9462e042240b878f02e53eec723f7
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2020-09-12 09:05:40 +0000

    Cleanup logging

--- Pods/Charts/Source/Charts/Charts/BarLineChartViewBase.swift
+++ Pods/Charts/Source/Charts/Charts/BarLineChartViewBase.swift
@@ -1601,4 +1601,22 @@ open class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChartD
             _scaleXEnabled = enabled
             _scaleYEnabled = enabled
             #if !os(tvOS)
-            _pinchGestureRecognizer.isEnabled = _pinchZoomEnabled || _scaleXEnabled || _scaleYEnable
+            _pinchGestureRecognizer.isEnabled = _pinchZoomEnabled || _scaleXEnabled || _scaleYEnabled
+            #endif
+        }
+    }
+    
+    @objc open var scaleXEnabled: Bool
+    {
+        get
+        {
+            return _scaleXEnabled
+        }
+        set
+        {
+            if _scaleXEnabled != newValue
+            {
+                _scaleXEnabled = newValue
+                #if !os(tvOS)
+                _pinchGestureRecognizer.isEnabled = _pinchZoomEnabled || _scaleXEnabled || _scaleYEnabled
+               
