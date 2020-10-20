9402c9be120ddb907e3f8e22fe0b28c0fffa01d4
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2020-10-20 06:42:14 +0000

    Minors.

--- Pods/Charts/Source/Charts/Charts/CandleStickChartView.swift
+++ Pods/Charts/Source/Charts/Charts/CandleStickChartView.swift
@@ -1,2 +1,22 @@
 //
-//  CandleStickC
+//  CandleStickChartView.swift
+//  Charts
+//
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+/// Financial chart type that draws candle-sticks.
+open class CandleStickChartView: BarLineChartViewBase, CandleChartDataProvider
+{
+    internal override func initialize()
+    {
+        super.initialize()
+        
+    
