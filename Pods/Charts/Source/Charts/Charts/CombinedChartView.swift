346344ccc4a0f1c2834f6dab8b24442c79008390
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2020-10-28 21:06:17 +0000

    updated filter

--- Pods/Charts/Source/Charts/Charts/CombinedChartView.swift
+++ Pods/Charts/Source/Charts/Charts/CombinedChartView.swift
@@ -12,4 +12,14 @@
 import Foundation
 import CoreGraphics
 
-/// This chart class allows the combination of lines, bars, scatter and candle data all displayed in one ch
+/// This chart class allows the combination of lines, bars, scatter and candle data all displayed in one chart area.
+open class CombinedChartView: BarLineChartViewBase, CombinedChartDataProvider
+{
+    /// the fill-formatter used for determining the position of the fill-line
+    internal var _fillFormatter: IFillFormatter!
+    
+    /// enum that allows to specify the order in which the different data objects for the combined-chart are drawn
+    @objc(CombinedChartDrawOrder)
+    public enum DrawOrder: Int
+    {
+        case bar
