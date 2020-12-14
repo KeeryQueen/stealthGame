fc640f2ce8ecee99a35a1c53d9b43d37ef9b9d66
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2020-12-14 18:46:44 +0000

    new filters

--- Pods/Charts/Source/Charts/Components/AxisBase.swift
+++ Pods/Charts/Source/Charts/Components/AxisBase.swift
@@ -28,4 +28,15 @@ open class AxisBase: ComponentBase
     @objc open var labelTextColor = NSUIColor.labelOrBlack
     
     @objc open var axisLineColor = NSUIColor.gray
-    @objc open var axisLineWidth = CGFloat(0.
+    @objc open var axisLineWidth = CGFloat(0.5)
+    @objc open var axisLineDashPhase = CGFloat(0.0)
+    @objc open var axisLineDashLengths: [CGFloat]!
+    
+    @objc open var gridColor = NSUIColor.gray.withAlphaComponent(0.9)
+    @objc open var gridLineWidth = CGFloat(0.5)
+    @objc open var gridLineDashPhase = CGFloat(0.0)
+    @objc open var gridLineDashLengths: [CGFloat]!
+    @objc open var gridLineCap = CGLineCap.butt
+    
+    @objc open var drawGridLinesEnabled = true
+    
