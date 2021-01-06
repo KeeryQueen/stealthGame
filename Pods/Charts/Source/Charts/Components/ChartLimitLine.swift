7061f72342b1abf0bf570d615ad718b550b27f6e
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-01-06 18:34:37 +0000

    change git ignore

--- Pods/Charts/Source/Charts/Components/ChartLimitLine.swift
+++ Pods/Charts/Source/Charts/Components/ChartLimitLine.swift
@@ -23,3 +23,53 @@ open class ChartLimitLine: ComponentBase
     {
         case topLeft
         case topRight
+        case bottomLeft
+        case bottomRight
+    }
+    
+    /// limit / maximum (the y-value or xIndex)
+    @objc open var limit = Double(0.0)
+    
+    private var _lineWidth = CGFloat(2.0)
+    @objc open var lineColor = NSUIColor(red: 237.0/255.0, green: 91.0/255.0, blue: 91.0/255.0, alpha: 1.0)
+    @objc open var lineDashPhase = CGFloat(0.0)
+    @objc open var lineDashLengths: [CGFloat]?
+    
+    @objc open var valueTextColor = NSUIColor.labelOrBlack
+    @objc open var valueFont = NSUIFont.systemFont(ofSize: 13.0)
+    
+    @objc open var drawLabelEnabled = true
+    @objc open var label = ""
+    @objc open var labelPosition = LabelPosition.topRight
+    
+    public override init()
+    {
+        super.init()
+    }
+    
+    @objc public init(limit: Double)
+    {
+        super.init()
+        self.limit = limit
+    }
+    
+    @objc public init(limit: Double, label: String)
+    {
+        super.init()
+        self.limit = limit
+        self.label = label
+    }
+    
+    /// set the line width of the chart (min = 0.2, max = 12); default 2
+    @objc open var lineWidth: CGFloat
+    {
+        get
+        {
+            return _lineWidth
+        }
+        set
+        {
+            _lineWidth = newValue.clamped(to: 0.2...12)
+        }
+    }
+}
---
+++ Pods/Charts/Source/Charts/Components/Description.swift
@@ -0,0 +1,23 @@
+//
+//  Description.swift
+//  Charts
+//
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+#if canImport(UIKit)
+    import UIKit
+#endif
+
+#if canImport(Cocoa)
+import Cocoa
+#endif
+
+@objc(ChartDescript
