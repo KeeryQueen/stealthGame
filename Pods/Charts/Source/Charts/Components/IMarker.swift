4bf207fbb1995be77bbd7074a6c25089271ad359
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-01-10 09:46:26 +0000

    updating references

--- Pods/Charts/Source/Charts/Components/IMarker.swift
+++ Pods/Charts/Source/Charts/Components/IMarker.swift
@@ -29,4 +29,9 @@ public protocol IMarker: class
     
     /// This method enables a custom IMarker to update it's content every time the IMarker is redrawn according to the data entry it points to.
     ///
-    /// - Param
+    /// - Parameters:
+    ///   - entry: The Entry the IMarker belongs to. This can also be any subclass of Entry, like BarEntry or CandleEntry, simply cast it at runtime.
+    ///   - highlight: The highlight object contains information about the highlighted value such as it's dataset-index, the selected range or stack-index (only stacked bar entries).
+    func refreshContent(entry: ChartDataEntry, highlight: Highlight)
+    
+    /// Draws th
