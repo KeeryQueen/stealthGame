1944a3340e1c0e602cba4885f2f64a25af736f04
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-04-04 18:32:17 +0000

    v0.2.16

--- Pods/Charts/Source/Charts/Components/YAxis.swift
+++ Pods/Charts/Source/Charts/Components/YAxis.swift
@@ -22,4 +22,18 @@ import Cocoa
 
 
 /// Class representing the y-axis labels settings and its entries.
-/// Be aware that not
+/// Be aware that not all features the YLabels class provides are suitable for the RadarChart.
+/// Customizations that affect the value range of the axis need to be applied before setting data for the chart.
+@objc(ChartYAxis)
+open class YAxis: AxisBase
+{
+    @objc(YAxisLabelPosition)
+    public enum LabelPosition: Int
+    {
+        case outsideChart
+        case insideChart
+    }
+    
+    ///  Enum that specifies the axis a DataSet should be plotted against, either Left or Right.
+    @objc
+    public enum AxisD
