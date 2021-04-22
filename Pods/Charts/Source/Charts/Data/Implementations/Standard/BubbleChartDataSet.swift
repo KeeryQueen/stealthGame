e0e0e9d40ac0c447d0c236335e82cd4aab221b4c
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-04-22 10:28:04 +0000

    YCP Killer: Apply changes needed for Ruby translation

--- Pods/Charts/Source/Charts/Data/Implementations/Standard/BubbleChartDataSet.swift
+++ Pods/Charts/Source/Charts/Data/Implementations/Standard/BubbleChartDataSet.swift
@@ -13,4 +13,16 @@ import Foundation
 import CoreGraphics
 
 
-open class BubbleChartDataSet: 
+open class BubbleChartDataSet: BarLineScatterCandleBubbleChartDataSet, IBubbleChartDataSet
+{
+    // MARK: - Data functions and accessors
+    
+    internal var _maxSize = CGFloat(0.0)
+    
+    open var maxSize: CGFloat { return _maxSize }
+    @objc open var normalizeSizeEnabled: Bool = true
+    open var isNormalizeSizeEnabled: Bool { return normalizeSizeEnabled }
+    
+    open override func calcMinMax(entry e: ChartDataEntry)
+    {
+        guard let e = e as? BubbleChartDat
