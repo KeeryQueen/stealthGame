9df4a0343dac442f25cdbbb4f576f2fca815f9e2
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-04-27 08:47:35 +0000

    v2.0.10

--- Pods/Charts/Source/Charts/Data/Implementations/Standard/ChartData.swift
+++ Pods/Charts/Source/Charts/Data/Implementations/Standard/ChartData.swift
@@ -265,3 +265,347 @@ open class ChartData: NSObject
             if _rightAxisMin == Double.greatestFiniteMagnitude
             {
                 return _leftAxisMin
+            }
+            else
+            {
+                return _rightAxisMin
+            }
+        }
+    }
+    
+    /// The greatest y-value the data object contains.
+    @objc open var yMax: Double
+    {
+        return _yMax
+    }
+    
+    @nonobjc
+    open func getYMax() -> Double
+    {
+        return _yMax
+    }
+    
+    @objc open func getYMax(axis: YAxis.AxisDependency) -> Double
+    {
+        if axis == .left
+        {
+            if _leftAxisMax == -Double.greatestFiniteMagnitude
+            {
+                return _rightAxisMax
+            }
+            else
+            {
+                return _leftAxisMax
+            }
+        }
+        else
+        {
+            if _rightAxisMax == -Double.greatestFiniteMagnitude
+            {
+                return _leftAxisMax
+            }
+            else
+            {
+                return _rightAxisMax
+            }
+        }
+    }
+    
+    /// The minimum x-value the data object contains.
+    @objc open var xMin: Double
+    {
+        return _xMin
+    }
+    /// The maximum x-value the data object contains.
+    @objc open var xMax: Double
+    {
+        return _xMax
+    }
+    
+    /// All DataSet objects this ChartData object holds.
+    @objc open var dataSets: [IChartDataSet]
+    {
+        get
+        {
+            return _dataSets
+        }
+        set
+        {
+            _dataSets = newValue
+            notifyDataChanged()
+        }
+    }
+    
+    /// Retrieve the index of a ChartDataSet with a specific label from the ChartData. Search can be case sensitive or not.
+    /// 
+    /// **IMPORTANT: This method does calculations at runtime, do not over-use in performance critical situations.**
+    ///
+    /// - Parameters:
+    ///   - dataSets: the DataSet array to search
+    ///   - type:
+    ///   - ignorecase: if true, the search is not case-sensitive
+    /// - Returns: The index of the DataSet Object with the given label. Sensitive or not.
+    internal func getDataSetIndexByLabel(_ label: String, ignorecase: Bool) -> Int
+    {
+        // TODO: Return nil instead of -1
+        if ignorecase
+        {
+            return dataSets.firstIndex { $0.label?.caseInsensitiveCompare(label) == .orderedSame }
+                ?? -1
+        }
+        else
+        {
+            return dataSets.firstIndex { $0.label == label }
+                ?? -1
+        }
+    }
+
+    /// Get the Entry for a corresponding highlight object
+    ///
+    /// - Parameters:
+    ///   - highlight:
+    /// - Returns: The entry that is highlighted
+    @objc open func entryForHighlight(_ highlight: Highlight) -> ChartDataEntry?
+    {
+        if highlight.dataSetIndex >= dataSets.count
+        {
+            return nil
+        }
+        else
+        {
+            return dataSets[highlight.dataSetIndex].entryForXValue(highlight.x, closestToY: highlight.y)
+        }
+    }
+    
+    /// **IMPORTANT: This method does calculations at runtime. Use with care in performance critical situations.**
+    ///
+    /// - Parameters:
+    ///   - label:
+    ///   - ignorecase:
+    /// - Returns: The DataSet Object with the given label. Sensitive or not.
+    @objc open func getDataSetByLabel(_ label: String, ignorecase: Bool) -> IChartDataSet?
+    {
+        let index = getDataSetIndexByLabel(label, ignorecase: ignorecase)
+        
+        if index < 0 || index >= _dataSets.count
+        {
+            return nil
+        }
+        else
+        {
+            return _dataSets[index]
+        }
+    }
+    
+    @objc open func getDataSetByIndex(_ index: Int) -> IChartDataSet!
+    {
+        if index < 0 || index >= _dataSets.count
+        {
+            return nil
+        }
+        
+        return _dataSets[index]
+    }
+    
+    @objc open func addDataSet(_ dataSet: IChartDataSet!)
+    {
+        calcMinMax(dataSet: dataSet)
+        
+        _dataSets.append(dataSet)
+    }
+    
+    /// Removes the given DataSet from this data object.
+    /// Also recalculates all minimum and maximum values.
+    ///
+    /// - Returns: `true` if a DataSet was removed, `false` ifno DataSet could be removed.
+    @objc @discardableResult open func removeDataSet(_ dataSet: IChartDataSet) -> Bool
+    {
+        guard let i = _dataSets.firstIndex(where: { $0 === dataSet }) else { return false }
+        return removeDataSetByIndex(i)
+    }
+    
+    /// Removes the DataSet at the given index in the DataSet array from the data object. 
+    /// Also recalculates all minimum and maximum values. 
+    ///
+    /// - Returns: `true` if a DataSet was removed, `false` ifno DataSet could be removed.
+    @objc @discardableResult open func removeDataSetByIndex(_ index: Int) -> Bool
+    {
+        if index >= _dataSets.count || index < 0
+        {
+            return false
+        }
+        
+        _dataSets.remove(at: index)
+        
+        calcMinMax()
+        
+        return true
+    }
+    
+    /// Adds an Entry to the DataSet at the specified index. Entries are added to the end of the list.
+    @objc open func addEntry(_ e: ChartDataEntry, dataSetIndex: Int)
+    {
+        if _dataSets.count > dataSetIndex && dataSetIndex >= 0
+        {
+            let set = _dataSets[dataSetIndex]
+            
+            if !set.addEntry(e) { return }
+            
+            calcMinMax(entry: e, axis: set.axisDependency)
+        }
+        else
+        {
+            print("ChartData.addEntry() - Cannot add Entry because dataSetIndex too high or too low.", terminator: "\n")
+        }
+    }
+    
+    /// Removes the given Entry object from the DataSet at the specified index.
+    @objc @discardableResult open func removeEntry(_ entry: ChartDataEntry, dataSetIndex: Int) -> Bool
+    {
+        // entry outofbounds
+        if dataSetIndex >= _dataSets.count
+        {
+            return false
+        }
+        
+        // remove the entry from the dataset
+        let removed = _dataSets[dataSetIndex].removeEntry(entry)
+        
+        if removed
+        {
+            calcMinMax()
+        }
+        
+        return removed
+    }
+    
+    /// Removes the Entry object closest to the given xIndex from the ChartDataSet at the
+    /// specified index. 
+    ///
+    /// - Returns: `true` if an entry was removed, `false` ifno Entry was found that meets the specified requirements.
+    @objc @discardableResult open func removeEntry(xValue: Double, dataSetIndex: Int) -> Bool
+    {
+        if dataSetIndex >= _dataSets.count
+        {
+            return false
+        }
+        
+        if let entry = _dataSets[dataSetIndex].entryForXValue(xValue, closestToY: Double.nan)
+        {
+            return removeEntry(entry, dataSetIndex: dataSetIndex)
+        }
+        
+        return false
+    }
+    
+    /// - Returns: The DataSet that contains the provided Entry, or null, if no DataSet contains this entry.
+    @objc open func getDataSetForEntry(_ e: ChartDataEntry) -> IChartDataSet?
+    {
+        return _dataSets.first { $0.entryForXValue(e.x, closestToY: e.y) === e }
+    }
+
+    /// - Returns: The index of the provided DataSet in the DataSet array of this data object, or -1 if it does not exist.
+    @objc open func indexOfDataSet(_ dataSet: IChartDataSet) -> Int
+    {
+        // TODO: Return nil instead of -1
+        return _dataSets.firstIndex { $0 === dataSet } ?? -1
+    }
+    
+    /// - Returns: The first DataSet from the datasets-array that has it's dependency on the left axis. Returns null if no DataSet with left dependency could be found.
+    @objc open func getFirstLeft(dataSets: [IChartDataSet]) -> IChartDataSet?
+    {
+        return dataSets.first { $0.axisDependency == .left }
+    }
+    
+    /// - Returns: The first DataSet from the datasets-array that has it's dependency on the right axis. Returns null if no DataSet with right dependency could be found.
+    @objc open func getFirstRight(dataSets: [IChartDataSet]) -> IChartDataSet?
+    {
+        return dataSets.first { $0.axisDependency == .right }
+    }
+    
+    /// - Returns: All colors used across all DataSet objects this object represents.
+    @objc open func getColors() -> [NSUIColor]?
+    {
+        // TODO: Don't return nil
+        return _dataSets.flatMap { $0.colors }
+    }
+    
+    /// Sets a custom IValueFormatter for all DataSets this data object contains.
+    @objc open func setValueFormatter(_ formatter: IValueFormatter)
+    {
+        dataSets.forEach { $0.valueFormatter = formatter }
+    }
+    
+    /// Sets the color of the value-text (color in which the value-labels are drawn) for all DataSets this data object contains.
+    @objc open func setValueTextColor(_ color: NSUIColor)
+    {
+        dataSets.forEach { $0.valueTextColor = color }
+    }
+    
+    /// Sets the font for all value-labels for all DataSets this data object contains.
+    @objc open func setValueFont(_ font: NSUIFont)
+    {
+        dataSets.forEach { $0.valueFont = font }
+    }
+
+    /// Enables / disables drawing values (value-text) for all DataSets this data object contains.
+    @objc open func setDrawValues(_ enabled: Bool)
+    {
+        dataSets.forEach { $0.drawValuesEnabled = enabled }
+    }
+    
+    /// Enables / disables highlighting values for all DataSets this data object contains.
+    /// If set to true, this means that values can be highlighted programmatically or by touch gesture.
+    @objc open var highlightEnabled: Bool
+    {
+        get { return dataSets.allSatisfy { $0.highlightEnabled } }
+        set { dataSets.forEach { $0.highlightEnabled = newValue } }
+    }
+    
+    /// if true, value highlightning is enabled
+    @objc open var isHighlightEnabled: Bool { return highlightEnabled }
+    
+    /// Clears this data object from all DataSets and removes all Entries.
+    /// Don't forget to invalidate the chart after this.
+    @objc open func clearValues()
+    {
+        dataSets.removeAll(keepingCapacity: false)
+        notifyDataChanged()
+    }
+    
+    /// Checks if this data object contains the specified DataSet. 
+    ///
+    /// - Returns: `true` if so, `false` ifnot.
+    @objc open func contains(dataSet: IChartDataSet) -> Bool
+    {
+        return dataSets.contains { $0 === dataSet }
+    }
+    
+    /// The total entry count across all DataSet objects this data object contains.
+    @objc open var entryCount: Int
+    {
+        return _dataSets.reduce(0) { $0 + $1.entryCount }
+    }
+
+    /// The DataSet object with the maximum number of entries or null if there are no DataSets.
+    @objc open var maxEntryCountSet: IChartDataSet?
+    {
+        return dataSets.max { $0.entryCount < $1.entryCount }
+    }
+
+    // MARK: - Accessibility
+
+    /// When the data entry labels are generated identifiers, set this property to prepend a string before each identifier
+    ///
+    /// For example, if a label is "#3", settings this property to "Item" allows it to be spoken as "Item #3"
+    @objc open var accessibilityEntryLabelPrefix: String?
+
+    /// When the data entry value requires a unit, use this property to append the string representation of the unit to the value
+    ///
+    /// For example, if a value is "44.1", setting this property to "m" allows it to be spoken as "44.1 m"
+    @objc open var accessibilityEntryLabelSuffix: String?
+
+    /// If the data entry value is a count, set this to true to allow plurals and other grammatical changes
+    /// **default**: false
+    @objc open var accessibilityEntryLabelSuffixIsCount: Bool = false
+}
---
+++ Pods/Charts/Source/Charts/Data/Implementations/Standard/ChartDataEntryBase.swift
@@ -0,0 +1,19 @@
+
+//
+//  ChartDataEntryBase.swift
+//  Charts
+//
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+
+open class ChartDataEntryBase: NSObject
+{
+    /// the y value
+    @objc open var y = 0.0
+    
