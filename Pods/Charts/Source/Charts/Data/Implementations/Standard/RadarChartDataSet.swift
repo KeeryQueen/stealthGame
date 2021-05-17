e377cfbe7d75d250ad253045e5fbe9fe3e460953
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-05-17 15:40:20 +0000

    more feedback

--- Pods/Charts/Source/Charts/Data/Implementations/Standard/RadarChartDataSet.swift
+++ Pods/Charts/Source/Charts/Data/Implementations/Standard/RadarChartDataSet.swift
@@ -2,4 +2,28 @@
 //  RadarChartDataSet.swift
 //  Charts
 //
-//  Copyright 2015 Daniel Cohen Gindi & Phil
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+
+open class RadarChartDataSet: LineRadarChartDataSet, IRadarChartDataSet
+{
+    private func initialize()
+    {
+        self.valueFont = NSUIFont.systemFont(ofSize: 13.0)
+    }
+    
+    public required init()
+    {
+        super.init()
+        initialize()
+    }
+    
+ 
