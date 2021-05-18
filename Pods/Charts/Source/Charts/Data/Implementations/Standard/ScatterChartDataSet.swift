4e14e95ccccbb35127fd2e342975219107edb640
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-05-18 22:21:13 +0000

    Fix line width

--- Pods/Charts/Source/Charts/Data/Implementations/Standard/ScatterChartDataSet.swift
+++ Pods/Charts/Source/Charts/Data/Implementations/Standard/ScatterChartDataSet.swift
@@ -30,4 +30,13 @@ open class ScatterChartDataSet: LineScatterCandleRadarChartDataSet, IScatterChar
     /// The size the scatter shape will have
     open var scatterShapeSize = CGFloat(10.0)
     
-    /// 
+    /// The radius of the hole in the shape (applies to Square, Circle and Triangle)
+    /// **default**: 0.0
+    open var scatterShapeHoleRadius: CGFloat = 0.0
+    
+    /// Color for the hole in the shape. Setting to `nil` will behave as transparent.
+    /// **default**: nil
+    open var scatterShapeHoleColor: NSUIColor? = nil
+    
+    /// Sets the ScatterShape this DataSet should be drawn with.
+    /// This will search for an availabl
