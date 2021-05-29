29eca9495760ff36ae11bbbe9f55b766fb07838a
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-05-29 19:48:43 +0000

    link fixes

--- Pods/Charts/Source/Charts/Data/Interfaces/IBubbleChartDataSet.swift
+++ Pods/Charts/Source/Charts/Data/Interfaces/IBubbleChartDataSet.swift
@@ -2,4 +2,22 @@
 //  IBubbleChartDataSet.swift
 //  Charts
 //
-//  Copyright 20
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+@objc
+public protocol IBubbleChartDataSet: IBarLineScatterCandleBubbleChartDataSet
+{
+    // MARK: - Data functions and accessors
+    
+    var maxSize: CGFloat { get }
+    var isNormalizeSizeEnabled: Bool { get }
+    
+    // MARK: - St
