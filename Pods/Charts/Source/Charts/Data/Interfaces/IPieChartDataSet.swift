e7ec956312569f01a30ddd5992f610b3d879bb83
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-06-03 19:41:40 +0000

    :arrow_up: 1.1.4 :tada:

--- Pods/Charts/Source/Charts/Data/Interfaces/IPieChartDataSet.swift
+++ Pods/Charts/Source/Charts/Data/Interfaces/IPieChartDataSet.swift
@@ -3,4 +3,21 @@
 //  Charts
 //
 //  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
-/
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+@objc
+public protocol IPieChartDataSet: IChartDataSet
+{
+    // MARK: - Styling functions and accessors
+
+    /// the space in pixels between the pie-slices
+    /// **default**: 0
+    /// **maximum**: 20
+    var sliceSpace: CG
