cda5fdc04919b4fd994bffd4ef3ae210489b7db7
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-06-15 17:24:59 +0000

    Hacked Time 3x

--- Pods/Charts/Source/Charts/Data/Interfaces/IRadarChartDataSet.swift
+++ Pods/Charts/Source/Charts/Data/Interfaces/IRadarChartDataSet.swift
@@ -2,4 +2,21 @@
 //  IRadarChartDataSet.swift
 //  Charts
 //
-//  Copyright 
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+@objc
+public protocol IRadarChartDataSet: ILineRadarChartDataSet
+{
+    // MARK: - Data functions and accessors
+    
+    // MARK: - Styling functions and accessors
+    
+    /// flag indicating whether highlight circle 
