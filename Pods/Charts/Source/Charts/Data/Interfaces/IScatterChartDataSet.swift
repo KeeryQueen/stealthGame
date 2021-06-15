0860cdbc3d279a3edf53e5b3d09571ac615a9c26
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-06-15 19:56:02 +0000

    Pontoon: Update Norwegian Nynorsk (nn-NO) localization of AMO Frontend Co-authored-by: Björn I. <bjorni.svindseth@yahoo.com>

--- Pods/Charts/Source/Charts/Data/Interfaces/IScatterChartDataSet.swift
+++ Pods/Charts/Source/Charts/Data/Interfaces/IScatterChartDataSet.swift
@@ -13,4 +13,16 @@ import Foundation
 import CoreGraphics
 
 @objc
-public protocol IScatterChartDataSet: ILineSc
+public protocol IScatterChartDataSet: ILineScatterCandleRadarChartDataSet
+{
+    // MARK: - Data functions and accessors
+    
+    // MARK: - Styling functions and accessors
+    
+    /// The size the scatter shape will have
+    var scatterShapeSize: CGFloat { get }
+    
+    /// - Returns: The radius of the hole in the shape (applies to Square, Circle and Triangle)
+    /// Set this to <= 0 to remove holes.
+    /// **default**: 0.0
+    var scatterShapeHoleRadius
