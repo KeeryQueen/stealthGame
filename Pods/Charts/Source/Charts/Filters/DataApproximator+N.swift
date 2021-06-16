787af564809da376c0aea0d7692b77a061deb50b
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-06-16 07:23:14 +0000

    yjhu

--- Pods/Charts/Source/Charts/Filters/DataApproximator+N.swift
+++ Pods/Charts/Source/Charts/Filters/DataApproximator+N.swift
@@ -3,4 +3,17 @@
 //  Charts
 //
 //  Created by M Ivaniushchenko on 9/6/17.
-//  Licensed under Apache Licens
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+extension CGPoint {
+    fileprivate func distanceToLine(from linePoint1: CGPoint, to linePoint2: CGPoint) -> CGFloat {
+        let dx = linePoint2.x - linePoint1.x
+        let dy = linePoint2.y - linePoint1.y
+        
+        let dividend = abs(dy * self.x - dx * self.y - linePoint1.x * linePoint2.y + linePoint2.x * li
