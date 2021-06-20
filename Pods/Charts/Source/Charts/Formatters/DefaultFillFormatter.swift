6074d8a9a639b1f6be8c2940b3301bd876c9d5d2
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-06-20 18:10:50 +0000

    Fixing pom

--- Pods/Charts/Source/Charts/Formatters/DefaultFillFormatter.swift
+++ Pods/Charts/Source/Charts/Formatters/DefaultFillFormatter.swift
@@ -2,4 +2,22 @@
 //  DefaultFillFormatter.swift
 //  Charts
 //
-//  Copyright 2015 Daniel Cohen Gindi
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+/// Default formatter that calculates the position of the filled line.
+@objc(ChartDefaultFillFormatter)
+open class DefaultFillFormatter: NSObject, IFillFormatter
+{
+    public typealias Block = (
+        _ dataSet: ILineChartDataSet,
+        _ dataProvider: LineChartDataProvider) -> CGFloat
+    
+    @o
