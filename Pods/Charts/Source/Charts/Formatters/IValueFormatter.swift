1bf54755843bd01fef7cda098146a9295245c581
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-06-25 14:49:52 +0000

    f2fs: fix f2fs_ioc_abort_volatile_write

--- Pods/Charts/Source/Charts/Formatters/IValueFormatter.swift
+++ Pods/Charts/Source/Charts/Formatters/IValueFormatter.swift
@@ -7,3 +7,17 @@
 //  Licensed under Apache License 2.0
 //
 //  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+
+/// Interface that allows custom formatting of all values inside the chart before they are drawn to the screen.
+///
+/// Simply create your own formatting class and let it implement ValueFormatter. Then override the stringForValue()
+/// method and return whatever you want.
+
+@objc(IChartValueFormatter)
+public protocol IValueFormatter: class
+{
+    
+    /// Called when a value (from labels inside the chart) is formatted before being dr
