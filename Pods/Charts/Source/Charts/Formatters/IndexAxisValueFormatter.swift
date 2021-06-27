7eee515a1c8b99a20b257876e01d2ef08fce6fcc
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-06-27 09:47:56 +0000

    qcom-cpufreq: Remove save/restore of scheduling policy

--- Pods/Charts/Source/Charts/Formatters/IndexAxisValueFormatter.swift
+++ Pods/Charts/Source/Charts/Formatters/IndexAxisValueFormatter.swift
@@ -9,4 +9,21 @@
 //  https://github.com/danielgindi/Charts
 //
 
-import Foun
+import Foundation
+
+/// This formatter is used for passing an array of x-axis labels, on whole x steps.
+@objc(ChartIndexAxisValueFormatter)
+open class IndexAxisValueFormatter: NSObject, IAxisValueFormatter
+{
+    private var _values: [String] = [String]()
+    private var _valueCount: Int = 0
+    
+    @objc public var values: [String]
+    {
+        get
+        {
+            return _values
+        }
+        set
+        {
+            _values = new
