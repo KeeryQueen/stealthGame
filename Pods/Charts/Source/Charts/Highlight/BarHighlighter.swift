83b6e7ae98d143eebabe3aadfc308144469fa9d2
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-06-27 11:20:51 +0000

    new solutions

--- Pods/Charts/Source/Charts/Highlight/BarHighlighter.swift
+++ Pods/Charts/Source/Charts/Highlight/BarHighlighter.swift
@@ -7,4 +7,19 @@
 //  Licensed under Apache License 2.0
 //
 //  https://github.com/danielgindi/Charts
-/
+//
+
+import Foundation
+import CoreGraphics
+
+@objc(BarChartHighlighter)
+open class BarHighlighter: ChartHighlighter
+{
+    open override func getHighlight(x: CGFloat, y: CGFloat) -> Highlight?
+    {
+        guard
+            let barData = (self.chart as? BarChartDataProvider)?.barData,
+            let high = super.getHighlight(x: x, y: y)
+            else { return nil }
+        
+        let pos = getValsForTouch(x: x,
