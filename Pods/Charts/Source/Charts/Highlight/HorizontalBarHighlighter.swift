8bf773c938d622bf95d485f847f70f14f1ff2e83
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-07-12 15:53:12 +0000

    Update doxygen

--- Pods/Charts/Source/Charts/Highlight/HorizontalBarHighlighter.swift
+++ Pods/Charts/Source/Charts/Highlight/HorizontalBarHighlighter.swift
@@ -1,2 +1,20 @@
 //
-//  HorizontalBarHighlighter.s
+//  HorizontalBarHighlighter.swift
+//  Charts
+//
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+@objc(HorizontalBarChartHighlighter)
+open class HorizontalBarHighlighter: BarHighlighter
+{
+    open override func getHighlight(x: CGFloat, y: CGFloat) -> Highlight?
+    {
+        guard let barData
