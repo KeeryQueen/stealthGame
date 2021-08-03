584fbc5af6ea94cd3c5e24c55cfcdb9dd7b040f2
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-08-03 20:30:53 +0000

    fix presence

--- Pods/Charts/Source/Charts/Highlight/PieRadarHighlighter.swift
+++ Pods/Charts/Source/Charts/Highlight/PieRadarHighlighter.swift
@@ -12,4 +12,15 @@
 import Foundation
 import CoreGraphics
 
-@objc(PieRadar
+@objc(PieRadarChartHighlighter)
+open class PieRadarHighlighter: ChartHighlighter
+{    
+    open override func getHighlight(x: CGFloat, y: CGFloat) -> Highlight?
+    {
+        guard let chart = self.chart as? PieRadarChartViewBase else { return nil }
+        
+        let touchDistanceToCenter = chart.distanceToCenter(x: x, y: y)
+        
+        // check if a slice was touched
+        guard touchDistanceToCenter <= chart.radius else
+        {
