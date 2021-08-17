d822d548dce82382a0b1346eb3031d3bcd423700
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-08-17 15:30:07 +0000

    f2fs: introduce f2fs_flush_merged_bios for cleanup

--- Pods/Charts/Source/Charts/Interfaces/ChartDataProvider.swift
+++ Pods/Charts/Source/Charts/Interfaces/ChartDataProvider.swift
@@ -2,4 +2,22 @@
 //  ChartDataProvider.swift
 //  Charts
 //
-//  Copyright 2015 Daniel Cohen Gindi & 
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+@objc
+public protocol ChartDataProvider
+{
+    /// The minimum x-value of the chart, regardless of zoom or translation.
+    var chartXMin: Double { get }
+    
+    /// The maximum x-value of the chart, regardless of zoom or translation.
+    var chartXMax: Double { get }
+    
