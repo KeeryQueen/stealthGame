f488fb1c8de99b23ca938ed5bdded969acdcbc9d
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-08-17 20:42:16 +0000

    updated package dependencies

--- Pods/Charts/Source/Charts/Interfaces/ChartDataProvider.swift
+++ Pods/Charts/Source/Charts/Interfaces/ChartDataProvider.swift
@@ -21,3 +21,18 @@ public protocol ChartDataProvider
     /// The maximum x-value of the chart, regardless of zoom or translation.
     var chartXMax: Double { get }
     
+    /// The minimum y-value of the chart, regardless of zoom or translation.
+    var chartYMin: Double { get }
+    
+    /// The maximum y-value of the chart, regardless of zoom or translation.
+    var chartYMax: Double { get }
+    
+    var maxHighlightDistance: CGFloat { get }
+    
+    var xRange: Double { get }
+    
+    var centerOffsets: CGPoint { get }
+    
+    var data: ChartData? { get }
+    
+    var maxVisibleCount: Int {
