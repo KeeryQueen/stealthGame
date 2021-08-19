417db3b4c625003a4faf359029f2d229eee0cfdd
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2021-08-19 23:09:55 +0000

    removed unnecessary line

--- Pods/Charts/Source/Charts/Jobs/ViewPortJob.swift
+++ Pods/Charts/Source/Charts/Jobs/ViewPortJob.swift
@@ -4,4 +4,20 @@
 //
 //  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
 //  A port of MPAndroidChart for iOS
-// 
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+// This defines a viewport modification job, used for delaying or animating viewport changes
+@objc(ChartViewPortJob)
+open class ViewPortJob: NSObject
+{
+    internal var point: CGPoint = CGPoint()
+    internal weak var viewPortHandler: ViewPortHandler?
+    internal var xValue: Double = 0.0
+    internal var yValue: Double = 0.0
+    internal weak var 
