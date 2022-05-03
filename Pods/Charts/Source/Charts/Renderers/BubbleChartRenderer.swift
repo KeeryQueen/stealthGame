e1dfdc1055552cabb4f9ccb8fc4dfa55077e9cca
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-05-03 12:48:36 +0000

    fixes #101

--- Pods/Charts/Source/Charts/Renderers/BubbleChartRenderer.swift
+++ Pods/Charts/Source/Charts/Renderers/BubbleChartRenderer.swift
@@ -6,4 +6,16 @@
 //    Copyright 2015 Pierre-Marc Airoldi
 //    Licensed under Apache License 2.0
 //
-//  https://gith
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+open class BubbleChartRenderer: BarLineScatterCandleBubbleRenderer
+{
+    /// A nested array of elements ordered logically (i.e not in visual/drawing order) for use with VoiceOver.
+    private lazy var accessibilityOrderedElements: [[NSUIAccessibilityElement]] = accessibilityCreateEmptyOrderedElements()
+
+    @objc open weak var dataProvider: BubbleChartDataProvider?
+    
