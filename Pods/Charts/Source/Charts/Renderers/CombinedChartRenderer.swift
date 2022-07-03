32845abbe9b4f1058973a1711938d3872ceb5641
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-07-03 19:32:03 +0000

    updated figures

--- Pods/Charts/Source/Charts/Renderers/CombinedChartRenderer.swift
+++ Pods/Charts/Source/Charts/Renderers/CombinedChartRenderer.swift
@@ -2,4 +2,19 @@
 //  CombinedChartRenderer.swift
 //  Charts
 //
-//  Copyright 2015 Da
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+open class CombinedChartRenderer: DataRenderer
+{
+    @objc open weak var chart: CombinedChartView?
+    
+    /// if set to true, all values are drawn above their bars, instead of below their top
+    @objc open var drawValueAboveBarEnab
