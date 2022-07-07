4c0c3270f6f856f91b52a4de729330c1d00e19b4
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-07-07 06:21:08 +0000

    Coding Standard:  - converted tabs to 4 spaces  - erased ending spaces

--- Pods/Charts/Source/Charts/Renderers/CombinedChartRenderer.swift
+++ Pods/Charts/Source/Charts/Renderers/CombinedChartRenderer.swift
@@ -17,4 +17,13 @@ open class CombinedChartRenderer: DataRenderer
     @objc open weak var chart: CombinedChartView?
     
     /// if set to true, all values are drawn above their bars, instead of below their top
-    @objc open var drawValueAboveBarEnab
+    @objc open var drawValueAboveBarEnabled = true
+    
+    /// if set to true, a grey area is drawn behind each bar that indicates the maximum value
+    @objc open var drawBarShadowEnabled = false
+    
+    internal var _renderers = [DataRenderer]()
+    
+    internal var _drawOrder: [CombinedChartView.DrawOrder] = [.bar, .bubble, .line, .candle, .scatter]
+    
+    @objc public init(chart: CombinedChartView, animator: Animator, viewPortHandler: ViewPortHandl
