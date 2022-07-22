cae6337ea4f0e189ac9471ebf27abc22d08579a6
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-07-22 12:47:43 +0000

    Refine README.md

--- Pods/Charts/Source/Charts/Renderers/LegendRenderer.swift
+++ Pods/Charts/Source/Charts/Renderers/LegendRenderer.swift
@@ -3,4 +3,22 @@
 //  Charts
 //
 //  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
-//  A port of MP
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+@objc(ChartLegendRenderer)
+open class LegendRenderer: Renderer
+{
+    /// the legend object this renderer renders
+    @objc open var legend: Legend?
+
+    @objc public init(viewPortHandler: ViewPortHandler, legend: Legend?)
+    {
+        super.init(viewPortHandler: viewPortHandler)
+ 
