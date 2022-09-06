5c3a4e753ffe8b89862ae9a625ce2b1c8384aa60
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-09-06 07:21:55 +0000

    sched/__wake_up_sync_key(): Fix nr_exclusive tasks which lead to WF_SYNC clearing

--- Pods/Charts/Source/Charts/Renderers/LineChartRenderer.swift
+++ Pods/Charts/Source/Charts/Renderers/LineChartRenderer.swift
@@ -570,3 +570,224 @@ open class LineChartRenderer: LineRadarRenderer
             guard let dataSet = lineData.getDataSetByIndex(i) as? ILineChartDataSet else { continue }
             
             if !dataSet.isVisible || dataSet.entryCount == 0
+            {
+                continue
+            }
+            
+            let trans = dataProvider.getTransformer(forAxis: dataSet.axisDependency)
+            let valueToPixelMatrix = trans.valueToPixelMatrix
+            
+            _xBounds.set(chart: dataProvider, dataSet: dataSet, animator: animator)
+            
+            let circleRadius = dataSet.circleRadius
+            let circleDiameter = circleRadius * 2.0
+            let circleHoleRadius = dataSet.circleHoleRadius
+            let circleHoleDiameter = circleHoleRadius * 2.0
+            
+            let drawCircleHole = dataSet.isDrawCircleHoleEnabled &&
+                circleHoleRadius < circleRadius &&
+                circleHoleRadius > 0.0
+            let drawTransparentCircleHole = drawCircleHole &&
+                (dataSet.circleHoleColor == nil ||
+                    dataSet.circleHoleColor == NSUIColor.clear)
+            
+            for j in _xBounds
+            {
+                guard let e = dataSet.entryForIndex(j) else { break }
+
+                pt.x = CGFloat(e.x)
+                pt.y = CGFloat(e.y * phaseY)
+                pt = pt.applying(valueToPixelMatrix)
+                
+                if (!viewPortHandler.isInBoundsRight(pt.x))
+                {
+                    break
+                }
+                
+                // make sure the circles don't do shitty things outside bounds
+                if (!viewPortHandler.isInBoundsLeft(pt.x) || !viewPortHandler.isInBoundsY(pt.y))
+                {
+                    continue
+                }
+                
+                
+                // Skip Circles and Accessibility if not enabled,
+                // reduces CPU significantly if not needed
+                if !dataSet.isDrawCirclesEnabled
+                {
+                    continue
+                }
+                
+                // Accessibility element geometry
+                let scaleFactor: CGFloat = 3
+                let accessibilityRect = CGRect(x: pt.x - (scaleFactor * circleRadius),
+                                               y: pt.y - (scaleFactor * circleRadius),
+                                               width: scaleFactor * circleDiameter,
+                                               height: scaleFactor * circleDiameter)
+                // Create and append the corresponding accessibility element to accessibilityOrderedElements
+                if let chart = dataProvider as? LineChartView
+                {
+                    let element = createAccessibleElement(withIndex: j,
+                                                          container: chart,
+                                                          dataSet: dataSet,
+                                                          dataSetIndex: i)
+                    { (element) in
+                        element.accessibilityFrame = accessibilityRect
+                    }
+
+                    accessibilityOrderedElements[i].append(element)
+                }
+
+                context.setFillColor(dataSet.getCircleColor(atIndex: j)!.cgColor)
+
+                rect.origin.x = pt.x - circleRadius
+                rect.origin.y = pt.y - circleRadius
+                rect.size.width = circleDiameter
+                rect.size.height = circleDiameter
+
+                if drawTransparentCircleHole
+                {
+                    // Begin path for circle with hole
+                    context.beginPath()
+                    context.addEllipse(in: rect)
+                    
+                    // Cut hole in path
+                    rect.origin.x = pt.x - circleHoleRadius
+                    rect.origin.y = pt.y - circleHoleRadius
+                    rect.size.width = circleHoleDiameter
+                    rect.size.height = circleHoleDiameter
+                    context.addEllipse(in: rect)
+                    
+                    // Fill in-between
+                    context.fillPath(using: .evenOdd)
+                }
+                else
+                {
+                    context.fillEllipse(in: rect)
+                    
+                    if drawCircleHole
+                    {
+                        context.setFillColor(dataSet.circleHoleColor!.cgColor)
+                     
+                        // The hole rect
+                        rect.origin.x = pt.x - circleHoleRadius
+                        rect.origin.y = pt.y - circleHoleRadius
+                        rect.size.width = circleHoleDiameter
+                        rect.size.height = circleHoleDiameter
+                        
+                        context.fillEllipse(in: rect)
+                    }
+                }
+            }
+        }
+        
+        context.restoreGState()
+
+        // Merge nested ordered arrays into the single accessibleChartElements.
+        accessibleChartElements.append(contentsOf: accessibilityOrderedElements.flatMap { $0 } )
+        accessibilityPostLayoutChangedNotification()
+    }
+    
+    open override func drawHighlighted(context: CGContext, indices: [Highlight])
+    {
+        guard
+            let dataProvider = dataProvider,
+            let lineData = dataProvider.lineData
+            else { return }
+        
+        let chartXMax = dataProvider.chartXMax
+        
+        context.saveGState()
+        
+        for high in indices
+        {
+            guard let set = lineData.getDataSetByIndex(high.dataSetIndex) as? ILineChartDataSet
+                , set.isHighlightEnabled
+                else { continue }
+            
+            guard let e = set.entryForXValue(high.x, closestToY: high.y) else { continue }
+            
+            if !isInBoundsX(entry: e, dataSet: set)
+            {
+                continue
+            }
+        
+            context.setStrokeColor(set.highlightColor.cgColor)
+            context.setLineWidth(set.highlightLineWidth)
+            if set.highlightLineDashLengths != nil
+            {
+                context.setLineDash(phase: set.highlightLineDashPhase, lengths: set.highlightLineDashLengths!)
+            }
+            else
+            {
+                context.setLineDash(phase: 0.0, lengths: [])
+            }
+            
+            let x = e.x // get the x-position
+            let y = e.y * Double(animator.phaseY)
+            
+            if x > chartXMax * animator.phaseX
+            {
+                continue
+            }
+            
+            let trans = dataProvider.getTransformer(forAxis: set.axisDependency)
+            
+            let pt = trans.pixelForValues(x: x, y: y)
+            
+            high.setDraw(pt: pt)
+            
+            // draw the lines
+            drawHighlightLines(context: context, point: pt, set: set)
+        }
+        
+        context.restoreGState()
+    }
+
+    /// Creates a nested array of empty subarrays each of which will be populated with NSUIAccessibilityElements.
+    /// This is marked internal to support HorizontalBarChartRenderer as well.
+    private func accessibilityCreateEmptyOrderedElements() -> [[NSUIAccessibilityElement]]
+    {
+        guard let chart = dataProvider as? LineChartView else { return [] }
+
+        let dataSetCount = chart.lineData?.dataSetCount ?? 0
+
+        return Array(repeating: [NSUIAccessibilityElement](),
+                     count: dataSetCount)
+    }
+
+    /// Creates an NSUIAccessibleElement representing the smallest meaningful bar of the chart
+    /// i.e. in case of a stacked chart, this returns each stack, not the combined bar.
+    /// Note that it is marked internal to support subclass modification in the HorizontalBarChart.
+    private func createAccessibleElement(withIndex idx: Int,
+                                          container: LineChartView,
+                                          dataSet: ILineChartDataSet,
+                                          dataSetIndex: Int,
+                                          modifier: (NSUIAccessibilityElement) -> ()) -> NSUIAccessibilityElement
+    {
+        let element = NSUIAccessibilityElement(accessibilityContainer: container)
+        let xAxis = container.xAxis
+
+        guard let e = dataSet.entryForIndex(idx) else { return element }
+        guard let dataProvider = dataProvider else { return element }
+
+        // NOTE: The formatter can cause issues when the x-axis labels are consecutive ints.
+        // i.e. due to the Double conversion, if there are more than one data set that are grouped,
+        // there is the possibility of some labels being rounded up. A floor() might fix this, but seems to be a brute force solution.
+        let label = xAxis.valueFormatter?.stringForValue(e.x, axis: xAxis) ?? "\(e.x)"
+
+        let elementValueText = dataSet.valueFormatter?.stringForValue(e.y,
+                                                                      entry: e,
+                                                                      dataSetIndex: dataSetIndex,
+                                                                      viewPortHandler: viewPortHandler) ?? "\(e.y)"
+
+        let dataSetCount = dataProvider.lineData?.dataSetCount ?? -1
+        let doesContainMultipleDataSets = dataSetCount > 1
+
+        element.accessibilityLabel = "\(doesContainMultipleDataSets ? (dataSet.label ?? "")  + ", " : "") \(label): \(elementValueText)"
+
+        modifier(element)
+
+        return element
+    }
+}
