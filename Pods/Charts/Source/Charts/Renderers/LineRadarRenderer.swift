7f267236114cc8ebafc6a1c1e54cc0343a6704b2
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-09-06 11:55:50 +0000

    adding file

--- Pods/Charts/Source/Charts/Renderers/LineRadarRenderer.swift
+++ Pods/Charts/Source/Charts/Renderers/LineRadarRenderer.swift
@@ -6,4 +6,19 @@
 //  A port of MPAndroidChart for iOS
 //  Licensed under Apache License 2.0
 //
-//  https://github.com/
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+@objc(LineRadarChartRenderer)
+open class LineRadarRenderer: LineScatterCandleRadarRenderer
+{
+    public override init(animator: Animator, viewPortHandler: ViewPortHandler)
+    {
+        super.init(animator: animator, viewPortHandler: viewPortHandler)
+    }
+    
+    /// Draws the provided path in filled mode with the provided drawable.
+    @objc open func drawFilledPath(context
