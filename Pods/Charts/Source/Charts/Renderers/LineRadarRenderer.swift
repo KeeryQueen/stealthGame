2aa374d8fc34c495a9470ea12b8fe6a5caa29371
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-09-07 15:33:47 +0000

    Update README file

--- Pods/Charts/Source/Charts/Renderers/LineRadarRenderer.swift
+++ Pods/Charts/Source/Charts/Renderers/LineRadarRenderer.swift
@@ -36,4 +36,17 @@ open class LineRadarRenderer: LineScatterCandleRadarRenderer
         context.restoreGState()
     }
     
-    /// Draws the provided path in filled mo
+    /// Draws the provided path in filled mode with the provided color and alpha.
+    @objc open func drawFilledPath(context: CGContext, path: CGPath, fillColor: NSUIColor, fillAlpha: CGFloat)
+    {
+        context.saveGState()
+        context.beginPath()
+        context.addPath(path)
+        
+        // filled is usually drawn with less alpha
+        context.setAlpha(fillAlpha)
+        
+        context.setFillColor(fillColor.cgColor)
+        context.fillPath()
+        
+        context.restoreG
