4054382b1d646eb4b5c5c3318ae3a4cd3e75d388
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-09-12 17:45:10 +0000

    prepared for release

--- Pods/Charts/Source/Charts/Renderers/RadarChartRenderer.swift
+++ Pods/Charts/Source/Charts/Renderers/RadarChartRenderer.swift
@@ -12,4 +12,13 @@
 import Foundation
 import CoreGraphics
 
-open class RadarChartRenderer: LineRadar
+open class RadarChartRenderer: LineRadarRenderer
+{
+    private lazy var accessibilityXLabels: [String] = {
+        guard let chart = chart else { return [] }
+        guard let formatter = chart.xAxis.valueFormatter else { return [] }
+
+        let maxEntryCount = chart.data?.maxEntryCountSet?.entryCount ?? 0
+        return stride(from: 0, to: maxEntryCount, by: 1).map {
+            formatter.stringForValue(Double($0), axis: chart.xAxis)
+      
