40657a0fe0da13d305c404a5bd9f9c2b67a51a97
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-11-11 14:08:37 +0000

    minor css fix

--- Pods/Charts/Source/Charts/Renderers/Scatter/ChevronDownShapeRenderer.swift
+++ Pods/Charts/Source/Charts/Renderers/Scatter/ChevronDownShapeRenderer.swift
@@ -23,4 +23,14 @@ open class ChevronDownShapeRenderer : NSObject, IShapeRenderer
         let shapeSize = dataSet.scatterShapeSize
         let shapeHalf = shapeSize / 2.0
         
-        context.setLineWidth
+        context.setLineWidth(1.0)
+        context.setStrokeColor(color.cgColor)
+        
+        context.beginPath()
+        context.move(to: CGPoint(x: point.x, y: point.y + 2 * shapeHalf))
+        context.addLine(to: CGPoint(x: point.x + 2 * shapeHalf, y: point.y))
+        context.move(to: CGPoint(x: point.x, y: point.y + 2 * shapeHalf))
+        context.addLine(to: CGPoint(x: point.x - 2 * shapeHalf, y: point.y))
+        context.strokePath()
+    }
+}
