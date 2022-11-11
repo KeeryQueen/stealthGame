24b852ee3ea74211eea6b0386062fac8ce84400d
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-11-11 13:50:58 +0000

    Migrate

--- Pods/Charts/Source/Charts/Renderers/Scatter/ChevronDownShapeRenderer.swift
+++ Pods/Charts/Source/Charts/Renderers/Scatter/ChevronDownShapeRenderer.swift
@@ -8,4 +8,19 @@
 //
 //  https://github.com/danielgindi/Charts
 //
-import Foundatio
+import Foundation
+import CoreGraphics
+
+open class ChevronDownShapeRenderer : NSObject, IShapeRenderer
+{
+    open func renderShape(
+        context: CGContext,
+                dataSet: IScatterChartDataSet,
+                viewPortHandler: ViewPortHandler,
+                point: CGPoint,
+                color: NSUIColor)
+    {
+        let shapeSize = dataSet.scatterShapeSize
+        let shapeHalf = shapeSize / 2.0
+        
+        context.setLineWidth
