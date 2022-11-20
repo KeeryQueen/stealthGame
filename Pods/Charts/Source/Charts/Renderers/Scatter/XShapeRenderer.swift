bd3baa6729fddd016b7137d0b304042f8f22bfc4
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-11-20 07:48:45 +0000

    Add German translation

--- Pods/Charts/Source/Charts/Renderers/Scatter/XShapeRenderer.swift
+++ Pods/Charts/Source/Charts/Renderers/Scatter/XShapeRenderer.swift
@@ -11,4 +11,19 @@
 import Foundation
 import CoreGraphics
 
-open c
+open class XShapeRenderer : NSObject, IShapeRenderer
+{
+    open func renderShape(
+        context: CGContext,
+        dataSet: IScatterChartDataSet,
+        viewPortHandler: ViewPortHandler,
+        point: CGPoint,
+        color: NSUIColor)
+    {
+        let shapeSize = dataSet.scatterShapeSize
+        let shapeHalf = shapeSize / 2.0
+        
+        context.setLineWidth(1.0)
+        context.setStrokeColor(color.cgColor)
+        
+     
