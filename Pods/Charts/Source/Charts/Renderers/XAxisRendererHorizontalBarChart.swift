150ea81c1de0ce964d091b8e05467ee35d46f16f
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-11-26 14:25:35 +0000

    modify tests

--- Pods/Charts/Source/Charts/Renderers/XAxisRendererHorizontalBarChart.swift
+++ Pods/Charts/Source/Charts/Renderers/XAxisRendererHorizontalBarChart.swift
@@ -6,4 +6,18 @@
 //  A port of MPAndroidChart for iOS
 //  Licensed under Apache License 2.0
 //
-//  https://github.com
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+open class XAxisRendererHorizontalBarChart: XAxisRenderer
+{
+    internal weak var chart: BarChartView?
+    
+    @objc public init(viewPortHandler: ViewPortHandler, xAxis: XAxis?, transformer: Transformer?, chart: BarChartView)
+    {
+        super.init(viewPortHandler: viewPortHandler, xAxis: xAxis, transformer: transformer)
+        
+        self.chart = c
