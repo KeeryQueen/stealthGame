cf83bb068dbc9b9a0e40c0fbe752c3eb719c1c1c
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-11-26 10:40:12 +0000

    added img

---
+++ Pods/Charts/Source/Charts/Renderers/XAxisRenderer.swift
@@ -0,0 +1,231 @@
+
+//
+//  XAxisRenderer.swift
+//  Charts
+//
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+#if canImport(UIKit)
+    import UIKit
+#endif
+
+#if canImport(Cocoa)
+import Cocoa
+#endif
+
+@objc(ChartXAxisRenderer)
+open class XAxisRenderer: AxisRendererBase
+{
+    @objc public init(viewPortHandler: ViewPortHandler, xAxis: XAxis?, transformer: Transformer?)
+    {
+        super.init(viewPortHandler: viewPortHandler, transformer: transformer, axis: xAxis)
+    }
+    
+    open override func computeAxis(min: Double, max: Double, inverted: Bool)
+    {
+        var min = min, max = max
+        
+        if let transformer = self.transformer
+        {
+            // calculate the starting and entry point of the y-labels (depending on
+            // zoom / contentrect bounds)
+            if viewPortHandler.contentWidth > 10 && !viewPortHandler.isFullyZoomedOutX
+            {
+                let p1 = transformer.valueForTouchPoint(CGPoint(x: viewPortHandler.contentLeft, y: viewPortHandler.contentTop))
+                let p2 = transformer.valueForTouchPoint(CGPoint(x: viewPortHandler.contentRight, y: viewPortHandler.contentTop))
+                
+                if inverted
+                {
+                    min = Double(p2.x)
+                    max = Double(p1.x)
+                }
+                else
+                {
+                    min = Double(p1.x)
+                    max = Double(p2.x)
+                }
+            }
+        }
+        
+        computeAxisValues(min: min, max: max)
+    }
+    
+    open override func computeAxisValues(min: Double, max: Double)
+    {
+        super.computeAxisValues(min: min, max: max)
+        
+        computeSize()
+    }
+    
+    @objc open func computeSize()
+    {
+        guard let
+            xAxis = self.axis as? XAxis
+            else { return }
+        
+        let longest = xAxis.getLongestLabel()
+        
+        let labelSize = longest.size(withAttributes: [NSAttributedString.Key.font: xAxis.labelFont])
+        
+        let labelWidth = labelSize.width
+        let labelHeight = labelSize.height
+        
+        let labelRotatedSize = labelSize.rotatedBy(degrees: xAxis.labelRotationAngle)
+        
+        xAxis.labelWidth = labelWidth
+        xAxis.labelHeight = labelHeight
+        xAxis.labelRotatedWidth = labelRotatedSize.width
+        xAxis.labelRotatedHeight = labelRotatedSize.height
+    }
+    
+    open override func renderAxisLabels(context: CGContext)
+    {
+        guard let xAxis = self.axis as? XAxis else { return }
+        
+        if !xAxis.isEnabled || !xAxis.isDrawLabelsEnabled
+        {
+            return
+        }
+        
+        let yOffset = xAxis.yOffset
+        
+        if xAxis.labelPosition == .top
+        {
+            drawLabels(context: context, pos: viewPortHandler.contentTop - yOffset, anchor: CGPoint(x: 0.5, y: 1.0))
+        }
+        else if xAxis.labelPosition == .topInside
+        {
+            drawLabels(context: context, pos: viewPortHandler.contentTop + yOffset + xAxis.labelRotatedHeight, anchor: CGPoint(x: 0.5, y: 1.0))
+        }
+        else if xAxis.labelPosition == .bottom
+        {
+            drawLabels(context: context, pos: viewPortHandler.contentBottom + yOffset, anchor: CGPoint(x: 0.5, y: 0.0))
+        }
+        else if xAxis.labelPosition == .bottomInside
+        {
+            drawLabels(context: context, pos: viewPortHandler.contentBottom - yOffset - xAxis.labelRotatedHeight, anchor: CGPoint(x: 0.5, y: 0.0))
+        }
+        else
+        { // BOTH SIDED
+            drawLabels(context: context, pos: viewPortHandler.contentTop - yOffset, anchor: CGPoint(x: 0.5, y: 1.0))
+            drawLabels(context: context, pos: viewPortHandler.contentBottom + yOffset, anchor: CGPoint(x: 0.5, y: 0.0))
+        }
+    }
+    
+    private var _axisLineSegmentsBuffer = [CGPoint](repeating: CGPoint(), count: 2)
+    
+    open override func renderAxisLine(context: CGContext)
+    {
+        guard let xAxis = self.axis as? XAxis else { return }
+        
+        if !xAxis.isEnabled || !xAxis.isDrawAxisLineEnabled
+        {
+            return
+        }
+        
+        context.saveGState()
+        
+        context.setStrokeColor(xAxis.axisLineColor.cgColor)
+        context.setLineWidth(xAxis.axisLineWidth)
+        if xAxis.axisLineDashLengths != nil
+        {
+            context.setLineDash(phase: xAxis.axisLineDashPhase, lengths: xAxis.axisLineDashLengths)
+        }
+        else
+        {
+            context.setLineDash(phase: 0.0, lengths: [])
+        }
+        
+        if xAxis.labelPosition == .top
+            || xAxis.labelPosition == .topInside
+            || xAxis.labelPosition == .bothSided
+        {
+            _axisLineSegmentsBuffer[0].x = viewPortHandler.contentLeft
+            _axisLineSegmentsBuffer[0].y = viewPortHandler.contentTop
+            _axisLineSegmentsBuffer[1].x = viewPortHandler.contentRight
+            _axisLineSegmentsBuffer[1].y = viewPortHandler.contentTop
+            context.strokeLineSegments(between: _axisLineSegmentsBuffer)
+        }
+        
+        if xAxis.labelPosition == .bottom
+            || xAxis.labelPosition == .bottomInside
+            || xAxis.labelPosition == .bothSided
+        {
+            _axisLineSegmentsBuffer[0].x = viewPortHandler.contentLeft
+            _axisLineSegmentsBuffer[0].y = viewPortHandler.contentBottom
+            _axisLineSegmentsBuffer[1].x = viewPortHandler.contentRight
+            _axisLineSegmentsBuffer[1].y = viewPortHandler.contentBottom
+            context.strokeLineSegments(between: _axisLineSegmentsBuffer)
+        }
+        
+        context.restoreGState()
+    }
+    
+    /// draws the x-labels on the specified y-position
+    @objc open func drawLabels(context: CGContext, pos: CGFloat, anchor: CGPoint)
+    {
+        guard
+            let xAxis = self.axis as? XAxis,
+            let transformer = self.transformer
+            else { return }
+        
+        let paraStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        paraStyle.alignment = .center
+        
+        let labelAttrs: [NSAttributedString.Key : Any] = [
+            .font: xAxis.labelFont,
+            .foregroundColor: xAxis.labelTextColor,
+            .paragraphStyle: paraStyle
+        ]
+        let labelRotationAngleRadians = xAxis.labelRotationAngle.DEG2RAD
+        
+        let centeringEnabled = xAxis.isCenterAxisLabelsEnabled
+
+        let valueToPixelMatrix = transformer.valueToPixelMatrix
+        
+        var position = CGPoint(x: 0.0, y: 0.0)
+        
+        var labelMaxSize = CGSize()
+        
+        if xAxis.isWordWrapEnabled
+        {
+            labelMaxSize.width = xAxis.wordWrapWidthPercent * valueToPixelMatrix.a
+        }
+        
+        let entries = xAxis.entries
+        
+        for i in stride(from: 0, to: entries.count, by: 1)
+        {
+            if centeringEnabled
+            {
+                position.x = CGFloat(xAxis.centeredEntries[i])
+            }
+            else
+            {
+                position.x = CGFloat(entries[i])
+            }
+            
+            position.y = 0.0
+            position = position.applying(valueToPixelMatrix)
+            
+            if viewPortHandler.isInBoundsX(position.x)
+            {
+                let label = xAxis.valueFormatter?.stringForValue(xAxis.entries[i], axis: xAxis) ?? ""
+
+                let labelns = label as NSString
+                
+                if xAxis.isAvoidFirstLastClippingEnabled
+                {
+                    // avoid clipping of the last
+                    if i == xAxis.entryCount - 1 && xAxis.entryCount > 1
+                    {
+                        let width = labelns.boundingRect(with: labelMaxSize, options: .usesLineFragmentOrigin, attributes: labelAttrs, context: nil).size.width
+                        
