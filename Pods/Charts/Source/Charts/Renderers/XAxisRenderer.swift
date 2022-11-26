32d5bf6294712a8ef5744912dbcf0fc42985aa12
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-11-26 13:12:14 +0000

    updated indentation

--- Pods/Charts/Source/Charts/Renderers/XAxisRenderer.swift
+++ Pods/Charts/Source/Charts/Renderers/XAxisRenderer.swift
@@ -229,3 +229,221 @@ open class XAxisRenderer: AxisRendererBase
                     {
                         let width = labelns.boundingRect(with: labelMaxSize, options: .usesLineFragmentOrigin, attributes: labelAttrs, context: nil).size.width
                         
+                        if width > viewPortHandler.offsetRight * 2.0
+                            && position.x + width > viewPortHandler.chartWidth
+                        {
+                            position.x -= width / 2.0
+                        }
+                    }
+                    else if i == 0
+                    { // avoid clipping of the first
+                        let width = labelns.boundingRect(with: labelMaxSize, options: .usesLineFragmentOrigin, attributes: labelAttrs, context: nil).size.width
+                        position.x += width / 2.0
+                    }
+                }
+                
+                drawLabel(context: context,
+                          formattedLabel: label,
+                          x: position.x,
+                          y: pos,
+                          attributes: labelAttrs,
+                          constrainedToSize: labelMaxSize,
+                          anchor: anchor,
+                          angleRadians: labelRotationAngleRadians)
+            }
+        }
+    }
+    
+    @objc open func drawLabel(
+        context: CGContext,
+        formattedLabel: String,
+        x: CGFloat,
+        y: CGFloat,
+        attributes: [NSAttributedString.Key : Any],
+        constrainedToSize: CGSize,
+        anchor: CGPoint,
+        angleRadians: CGFloat)
+    {
+        ChartUtils.drawMultilineText(
+            context: context,
+            text: formattedLabel,
+            point: CGPoint(x: x, y: y),
+            attributes: attributes,
+            constrainedToSize: constrainedToSize,
+            anchor: anchor,
+            angleRadians: angleRadians)
+    }
+    
+    open override func renderGridLines(context: CGContext)
+    {
+        guard
+            let xAxis = self.axis as? XAxis,
+            let transformer = self.transformer
+            else { return }
+        
+        if !xAxis.isDrawGridLinesEnabled || !xAxis.isEnabled
+        {
+            return
+        }
+        
+        context.saveGState()
+        defer { context.restoreGState() }
+        context.clip(to: self.gridClippingRect)
+        
+        context.setShouldAntialias(xAxis.gridAntialiasEnabled)
+        context.setStrokeColor(xAxis.gridColor.cgColor)
+        context.setLineWidth(xAxis.gridLineWidth)
+        context.setLineCap(xAxis.gridLineCap)
+        
+        if xAxis.gridLineDashLengths != nil
+        {
+            context.setLineDash(phase: xAxis.gridLineDashPhase, lengths: xAxis.gridLineDashLengths)
+        }
+        else
+        {
+            context.setLineDash(phase: 0.0, lengths: [])
+        }
+        
+        let valueToPixelMatrix = transformer.valueToPixelMatrix
+        
+        var position = CGPoint(x: 0.0, y: 0.0)
+        
+        let entries = xAxis.entries
+        
+        for i in stride(from: 0, to: entries.count, by: 1)
+        {
+            position.x = CGFloat(entries[i])
+            position.y = position.x
+            position = position.applying(valueToPixelMatrix)
+            
+            drawGridLine(context: context, x: position.x, y: position.y)
+        }
+    }
+    
+    @objc open var gridClippingRect: CGRect
+    {
+        var contentRect = viewPortHandler.contentRect
+        let dx = self.axis?.gridLineWidth ?? 0.0
+        contentRect.origin.x -= dx / 2.0
+        contentRect.size.width += dx
+        return contentRect
+    }
+    
+    @objc open func drawGridLine(context: CGContext, x: CGFloat, y: CGFloat)
+    {
+        if x >= viewPortHandler.offsetLeft
+            && x <= viewPortHandler.chartWidth
+        {
+            context.beginPath()
+            context.move(to: CGPoint(x: x, y: viewPortHandler.contentTop))
+            context.addLine(to: CGPoint(x: x, y: viewPortHandler.contentBottom))
+            context.strokePath()
+        }
+    }
+    
+    open override func renderLimitLines(context: CGContext)
+    {
+        guard
+            let xAxis = self.axis as? XAxis,
+            let transformer = self.transformer,
+            !xAxis.limitLines.isEmpty
+            else { return }
+        
+        let trans = transformer.valueToPixelMatrix
+        
+        var position = CGPoint(x: 0.0, y: 0.0)
+        
+        for l in xAxis.limitLines where l.isEnabled
+        {
+            context.saveGState()
+            defer { context.restoreGState() }
+            
+            var clippingRect = viewPortHandler.contentRect
+            clippingRect.origin.x -= l.lineWidth / 2.0
+            clippingRect.size.width += l.lineWidth
+            context.clip(to: clippingRect)
+            
+            position.x = CGFloat(l.limit)
+            position.y = 0.0
+            position = position.applying(trans)
+            
+            renderLimitLineLine(context: context, limitLine: l, position: position)
+            renderLimitLineLabel(context: context, limitLine: l, position: position, yOffset: 2.0 + l.yOffset)
+        }
+    }
+    
+    @objc open func renderLimitLineLine(context: CGContext, limitLine: ChartLimitLine, position: CGPoint)
+    {
+        
+        context.beginPath()
+        context.move(to: CGPoint(x: position.x, y: viewPortHandler.contentTop))
+        context.addLine(to: CGPoint(x: position.x, y: viewPortHandler.contentBottom))
+        
+        context.setStrokeColor(limitLine.lineColor.cgColor)
+        context.setLineWidth(limitLine.lineWidth)
+        if limitLine.lineDashLengths != nil
+        {
+            context.setLineDash(phase: limitLine.lineDashPhase, lengths: limitLine.lineDashLengths!)
+        }
+        else
+        {
+            context.setLineDash(phase: 0.0, lengths: [])
+        }
+        
+        context.strokePath()
+    }
+    
+    @objc open func renderLimitLineLabel(context: CGContext, limitLine: ChartLimitLine, position: CGPoint, yOffset: CGFloat)
+    {
+        
+        let label = limitLine.label
+        guard limitLine.drawLabelEnabled, !label.isEmpty else { return }
+
+        let labelLineHeight = limitLine.valueFont.lineHeight
+
+        let xOffset: CGFloat = limitLine.lineWidth + limitLine.xOffset
+        let attributes: [NSAttributedString.Key : Any] = [
+            .font : limitLine.valueFont,
+            .foregroundColor : limitLine.valueTextColor
+        ]
+
+        let (point, align): (CGPoint, NSTextAlignment)
+        switch limitLine.labelPosition {
+        case .topRight:
+            point = CGPoint(
+                x: position.x + xOffset,
+                y: viewPortHandler.contentTop + yOffset
+            )
+            align = .left
+
+        case .bottomRight:
+            point = CGPoint(
+                x: position.x + xOffset,
+                y: viewPortHandler.contentBottom - labelLineHeight - yOffset
+            )
+            align = .left
+
+        case .topLeft:
+            point = CGPoint(
+                x: position.x - xOffset,
+                y: viewPortHandler.contentTop + yOffset
+            )
+            align = .right
+
+        case .bottomLeft:
+            point = CGPoint(
+                x: position.x - xOffset,
+                y: viewPortHandler.contentBottom - labelLineHeight - yOffset
+            )
+            align = .right
+        }
+
+        ChartUtils.drawText(
+            context: context,
+            text: label,
+            point: point,
+            align: align,
+            attributes: attributes
+        )
+    }
+}
