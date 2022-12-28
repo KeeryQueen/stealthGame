097464e1d1f8222209a8455d5bf684df73c11acc
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2022-12-28 13:01:44 +0000

    Reverting.

--- Pods/Charts/Source/Charts/Renderers/YAxisRendererRadarChart.swift
+++ Pods/Charts/Source/Charts/Renderers/YAxisRendererRadarChart.swift
@@ -4,4 +4,26 @@
 //
 //  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
 //  A port of MPAndroidChart for iOS
-//  Licensed under Apache Lic
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+#if canImport(UIKit)
+    import UIKit
+#endif
+
+#if canImport(Cocoa)
+import Cocoa
+#endif
+
+open class YAxisRendererRadarChart: YAxisRenderer
+{
+    private weak var chart: RadarChartView?
+    
+    @objc public init(viewPortHandler: ViewPortHandler, yAxis: YAxis?, chart: RadarChartView)
+    {
+        super.init(viewPortHandler: viewPortHandler, yAxis: yAxis, tra
