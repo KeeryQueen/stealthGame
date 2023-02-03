16963abae2a43378d3632f6db211488793a93c54
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2023-02-03 13:03:47 +0000

    Delete bg.jpg

--- Pods/Charts/Source/Charts/Utils/Platform.swift
+++ Pods/Charts/Source/Charts/Utils/Platform.swift
@@ -1,4 +1,17 @@
 import Foundation
 
 /** This file provides a thin abstraction layer atop of UIKit (iOS, tvOS) and Cocoa (OS X). The two APIs are very much 
- alike, and for the chart library's usage of the APIs it is often sufficient to typealias one to the oth
+ alike, and for the chart library's usage of the APIs it is often sufficient to typealias one to the other. The NSUI*
+ types are aliased to either their UI* implementation (on iOS) or their NS* implementation (on OS X). */
+#if os(iOS) || os(tvOS)
+#if canImport(UIKit)
+    import UIKit
+#endif
+
+public typealias NSUIFont = UIFont
+public typealias NSUIImage = UIImage
+public typealias NSUIScrollView = UIScrollView
+public typealias NSUIScreen = UIScreen
+public typealias NSUIDisplayLink = CADisplayLink
+
+open class NSUIView: UIView
