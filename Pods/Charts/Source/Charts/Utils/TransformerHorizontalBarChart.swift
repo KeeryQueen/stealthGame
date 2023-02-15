8c76bd53fdb1bffe178989fe37fdc34432902486
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2023-02-15 17:48:01 +0000

    remove cruft

--- Pods/Charts/Source/Charts/Utils/TransformerHorizontalBarChart.swift
+++ Pods/Charts/Source/Charts/Utils/TransformerHorizontalBarChart.swift
@@ -1,2 +1,20 @@
 //
-//  TransformerHorizontalBarChart.swi
+//  TransformerHorizontalBarChart.swift
+//  Charts
+//
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+import CoreGraphics
+
+@objc(ChartTransformerHorizontalBarChart)
+open class TransformerHorizontalBarChart: Transformer
+{
+    /// Prepares the matrix that contains all offsets.
+    open override func prepareMatrixOffset(inverted: Bool)
+  
