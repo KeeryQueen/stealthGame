2f6f595419373824d793cc1646c21df7ff5f2e32
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2023-02-19 10:57:30 +0000

    Update history.md

--- Pods/Charts/Source/Charts/Utils/TransformerHorizontalBarChart.swift
+++ Pods/Charts/Source/Charts/Utils/TransformerHorizontalBarChart.swift
@@ -17,4 +17,13 @@ open class TransformerHorizontalBarChart: Transformer
 {
     /// Prepares the matrix that contains all offsets.
     open override func prepareMatrixOffset(inverted: Bool)
-  
+    {
+        if !inverted
+        {
+            _matrixOffset = CGAffineTransform(translationX: _viewPortHandler.offsetLeft, y: _viewPortHandler.chartHeight - _viewPortHandler.offsetBottom)
+        }
+        else
+        {
+            _matrixOffset = CGAffineTransform(scaleX: -1.0, y: 1.0)
+            _matrixOffset = _matrixOffset.translatedBy(x: -(_viewPortHandler.chartWidth - _viewPortHandler.offsetRight),
+              
