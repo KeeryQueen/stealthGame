d8e07b3b0ec383402c57489361e7ff16b94f4c4a
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2023-02-22 07:25:31 +0000

    Update roles

--- Pods/Charts/Source/Charts/Utils/TransformerHorizontalBarChart.swift
+++ Pods/Charts/Source/Charts/Utils/TransformerHorizontalBarChart.swift
@@ -26,4 +26,7 @@ open class TransformerHorizontalBarChart: Transformer
         {
             _matrixOffset = CGAffineTransform(scaleX: -1.0, y: 1.0)
             _matrixOffset = _matrixOffset.translatedBy(x: -(_viewPortHandler.chartWidth - _viewPortHandler.offsetRight),
-              
+                y: _viewPortHandler.chartHeight - _viewPortHandler.offsetBottom)
+        }
+    }
+}
