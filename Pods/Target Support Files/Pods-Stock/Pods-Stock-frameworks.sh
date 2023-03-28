140118188e5fc1b6ea5860405896a04c2f84a3f6
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2023-03-28 20:41:23 +0000

    development update

---
+++ Pods/Target Support Files/Pods-Stock/Pods-Stock-frameworks.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+set -u
+set -o pipefail
+
+function on_error {
+  echo "$(realpath -mq "${0}"):$1: error: Unexpected failure"
+}
+trap 'on_error $LINENO' ERR
+
+if [ -z ${FRAMEWORKS_FOLDER_PATH+x} ]; then
+  # If FRAMEWORKS_FOLDER_PATH is not set, then there's nowhere for us to copy
+  # frameworks to, so exit 0 (signalling the script phase was successful)
