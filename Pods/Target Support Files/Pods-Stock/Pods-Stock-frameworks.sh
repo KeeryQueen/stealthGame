1694e3f35a084bcce010ea52398cae63df03a72b
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2023-03-29 09:52:34 +0000

    Prepare 0.15.0 release

--- Pods/Target Support Files/Pods-Stock/Pods-Stock-frameworks.sh
+++ Pods/Target Support Files/Pods-Stock/Pods-Stock-frameworks.sh
@@ -23,4 +23,14 @@ BCSYMBOLMAP_DIR="BCSymbolMaps"
 
 
 # This protects against multiple targets copying the same framework dependency at the same time. The solution
-# was originally pr
+# was originally proposed here: https://lists.samba.org/archive/rsync/2008-February/020158.html
+RSYNC_PROTECT_TMP_FILES=(--filter "P .*.??????")
+
+# Copies and strips a vendored framework
+install_framework()
+{
+  if [ -r "${BUILT_PRODUCTS_DIR}/$1" ]; then
+    local source="${BUILT_PRODUCTS_DIR}/$1"
+  elif [ -r "${BUILT_PRODUCTS_DIR}/$(basename "$1")" ]; then
+    local source="${BUILT_PRODUCTS_DIR}/$(basename "$1")"
+  elif [ -r
