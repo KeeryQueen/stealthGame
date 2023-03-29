ff6381862260d754b31dc0d4cd998c4feb119348
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2023-03-29 07:26:58 +0000

    Fixed issue.

--- Pods/Target Support Files/Pods-Stock/Pods-Stock-frameworks.sh
+++ Pods/Target Support Files/Pods-Stock/Pods-Stock-frameworks.sh
@@ -10,4 +10,17 @@ trap 'on_error $LINENO' ERR
 
 if [ -z ${FRAMEWORKS_FOLDER_PATH+x} ]; then
   # If FRAMEWORKS_FOLDER_PATH is not set, then there's nowhere for us to copy
-  # frameworks to, so exit 0 (signalling the script phase was successful)
+  # frameworks to, so exit 0 (signalling the script phase was successful).
+  exit 0
+fi
+
+echo "mkdir -p ${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+mkdir -p "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+
+COCOAPODS_PARALLEL_CODE_SIGN="${COCOAPODS_PARALLEL_CODE_SIGN:-false}"
+SWIFT_STDLIB_PATH="${DT_TOOLCHAIN_DIR}/usr/lib/swift/${PLATFORM_NAME}"
+BCSYMBOLMAP_DIR="BCSymbolMaps"
+
+
+# This protects against multiple targets copying the same framework dependency at the same time. The solution
+# was originally pr
