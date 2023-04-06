fe8f253f68a9763040f34ecbb8becf30019e3c56
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2023-04-06 21:50:03 +0000

    Version 0.12

--- Pods/Target Support Files/Pods-Stock/Pods-Stock-frameworks.sh
+++ Pods/Target Support Files/Pods-Stock/Pods-Stock-frameworks.sh
@@ -174,4 +174,12 @@ code_sign_if_enabled() {
   fi
 }
 
-if [[ "$CONFIGURATION
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
+fi
+if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
+  wait
+fi
