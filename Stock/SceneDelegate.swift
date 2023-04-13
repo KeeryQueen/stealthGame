5d8071ec0658e7f85f5b3790f8cb3655e6f5a758
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2023-04-13 05:44:14 +0000

    Bump version to 0.10.0

--- Stock/SceneDelegate.swift
+++ Stock/SceneDelegate.swift
@@ -12,4 +12,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSe
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = UINavigationController(rootViewController: StockListVC())
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScen
