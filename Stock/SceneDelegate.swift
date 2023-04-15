48e622a6f4322804682a4584e878453e81ed8a75
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2023-04-15 08:46:01 +0000

    adjust comments

--- Stock/SceneDelegate.swift
+++ Stock/SceneDelegate.swift
@@ -36,4 +36,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interr
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+  
