886f776b86b90e194ad2b6a48a8dc62c7c383023
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2023-04-15 08:47:06 +0000

    :robot: Webmentions updated by Mastrl Cntrl

---
+++ Stock/StockCell.swift
@@ -0,0 +1,15 @@
+//
+//  StockCell.swift
+//  Stock
+//
+//  Created by 張永霖 on 2021/5/6.
+//
+
+import UIKit
+
+class StockCell: UITableViewCell {
+
+    static let reuseID = "StockCell"
+    let codenameLabel = UILabel()
+    let typeCurrencyLabel = UILabel()
+    let fullnameLabel
