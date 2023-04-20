500a2e90c53e06f5fd2bb73af3c96d2899a69648
KeeryQueen <106528746+KeeryQueen@users.noreply.github.com>
2023-04-20 09:41:52 +0000

    added more details

--- Stock/Utility/ErrorMessage.swift
+++ Stock/Utility/ErrorMessage.swift
@@ -1,2 +1,16 @@
 //
 //  ErrorMessage.swift
+//  Stock
+//
+//  Created by 張永霖 on 2021/5/6.
+//
+
+import Foundation
+
+enum ErrorMessage: String, Error {
+    case connectionFaild = "Ineternet connection failed. Please try again."
+    case invalidURL = "URL seems to have some problem."
+    case unableToComplete = "Unable to complete."
+    case invalidData = "Something about data went wrong."
+    case invalidDecodeData = "Something about data went wrong at decoding."
+}
